b09b868b1df3a04d6c6911d3d843df2564544113
anish-gupta-0807 <anishgupta487@gmail.com>
2026-04-07 23:12:26 +0530

    Adding http basic

--- src/main/java/com/example/TaskManager/config/SecurityConfig.java
+++ src/main/java/com/example/TaskManager/config/SecurityConfig.java
@@ -3,11 +3,11 @@ package com.example.TaskManager.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -16,9 +16,9 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
-                        .anyRequest().authenticated()
-                );
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(withDefaults());
 
         return http.build();
     }
