be4dbd579d97c7021ecd2cef44eed6db33d3e8f5
anish-gupta-0807 <anishgupta487@gmail.com>
2026-04-07 23:09:20 +0530

    username should be unique

--- src/main/java/com/example/TaskManager/entity/User.java
+++ src/main/java/com/example/TaskManager/entity/User.java
@@ -1,9 +1,6 @@
 package com.example.TaskManager.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.service.annotation.GetExchange;
@@ -15,7 +12,8 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(unique = true)
     String username;
+
     String password;
 }
